7d6326b4cd23f6ad6577af400fcacbd4746cddac
visheshg20 <visheshg20@gmail.com>
2024-06-04 07:42:30 -0700

    change name

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Flock",
+  description: "The best way to make new friends at Stanford",
 };
 
 const poppins = Poppins({
